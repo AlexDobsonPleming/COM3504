79fc410ec89a466777625f81fb1e7d2d586b47a0
Eleanor Higgs <elhiggs1@sheffield.ac.uk>
2024-04-27 18:23:06 +0100

    test caching and fixed lat long

--- routes/index.mjs
+++ routes/index.mjs
@@ -45,7 +45,7 @@ router.post('/add', upload.single('myImg'), function (req, res, next) {
         date_time_seen: date_time_seen,
         plant_width: plant_width,
         plant_height: plant_height,
-        plant_location: {lat: plant_lat, long: plant_long },
+        plant_location: {lat: latitude, long: longitude },
         has_flowers: has_flowers,
         has_fruit: has_fruit,
         has_seeds: has_seeds,
