4c077900cd7f0ffdc3fd8d29e99cf0ed2dbf002e
Eleanor Higgs <elhiggs1@sheffield.ac.uk>
2024-04-27 18:35:19 +0100

    cache addplant

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -2,7 +2,7 @@ console.log('sw called');
 self.addEventListener('install', (event ) => {
     console.log('[Service Worker] : Installed');
     event.waitUntil(caches.open('core').then((cache ) => {
-        cache.add(new Request('test.html')).then(() => {
+        cache.add(new Request('addplant.ejs')).then(() => {
             console.log('[Service Worker] : Cached test')
         });
     }));
@@ -11,12 +11,12 @@ self.addEventListener('install', (event ) => {
 self.addEventListener('fetch', (event) => {
     console.log('[Service Worker] : Fetching');
     const request = event.request;
-    if (request.headers.get("Accept").includes("text/html")) {
+    if (request.headers.get("Accept")) {
         event.respondWith(
             fetch(request).then((response ) => {
                 return response;
             }).catch(() => {
-                return caches.match('test.html');
+                return caches.match('addplant.ejs');
             }));
     }
 });
