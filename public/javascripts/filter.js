49351619c9bbd8f87b20a4416162987b6435304c
Anastasiia Yatluk <ayatlukbondarchuk1@sheffield.ac.uk>
2024-04-27 21:45:18 +0100

    home page view finished, just lacks functionality-linking to database

---
+++ public/javascripts/filter.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const filterButtons = document.querySelectorAll('.filter-section button');
+
+    filterButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            filterButtons.forEach(btn => btn.classList.remove('active')); // Remove active class from all buttons
+            this.classList.add('active'); // Add active class to the clicked button
+        });
+    });
+});
