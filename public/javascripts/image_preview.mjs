ca95ea441068df3e3bdab22a2541614d4b91b39f
Alexander Dobson-Pleming <alexdobsonpleming@gmail.com>
2024-05-03 14:55:29 +0100

    Moved script references out to appropriate views instead of layout and converted them to ES6 modules

---
+++ public/javascripts/image_preview.mjs
@@ -0,0 +1,17 @@
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('myImage').addEventListener('change', function(event) {
+        if (event.target.files && event.target.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                // Get the existing image element by ID
+                var previewImage = document.getElementById('previewImage');
+                // Set its source to the current file's Data URL
+                previewImage.src = e.target.result;
+            };
+            // Read the file as a Data URL
+            reader.readAsDataURL(event.target.files[0]);
+        }
+    });
+});
